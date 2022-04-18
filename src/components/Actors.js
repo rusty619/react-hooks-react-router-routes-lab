771ae41a2302d28983af33c877f0f08d9db5a5ad
Ruslan Khait <rusty6916@gmail.com>
2022-04-17 21:53:49 -0400

    commit tree 74ef9348ed1c7cc8fed55c9aaebb476600df4303

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <div>
+      <h1>Actors Page</h1>
+      {
+        actors.map((actor) => {
+          return(
+            <div>
+              <h2>Name: {actor.name}</h2>
+                  <ul>
+                    Movies: 
+                    {
+                      actor.movies.map((movie) => {
+                        return(<li>{movie}</li>)
+                      })
+                    }
+                  </ul>
+            </div>)
+        })
+      }
+    </div>
+    );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+   <div>
+    <h1>Movie Page</h1>
+    {
+      movies.map((element) => {
+        return(
+         <div>
+           <h1>Name: {element.title}</h1>
+            <p>Time: {element.time}</p>
+            <p>Genres: </p>
+            <ul>
+              {
+                element.genres.map((genre) => {
+                  return(<li>{genre}</li>)
+                })
+              }
+            </ul>
+         </div>
+         )
+      })
+    }
+   </div>
+   );
 }
 
 export default Movies;
